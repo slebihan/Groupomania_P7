0fb15c0b41773d7b58e5f3bb194008a45ace0fa8
Stephanielebihan <94453807+Stephanielebihan@users.noreply.github.com>
2022-06-06 08:59:32 +0200

    last update routes

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 const commentsCtrl = require('../controllers/commentsCtrl');
 
 router.post("/comments/",multer,commentsCtrl.createComment);
-router.get("/comments/",multer,commentsCtrl.getAllComments);
-router.delete("/comments/:id",commentsCtrl.deleteComment)
+router.get("/comments/",commentsCtrl.getAllComments);
+router.delete("/comments/:id",auth,commentsCtrl.deleteComment)
 
 
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,9 +11,9 @@ const userCtrl = require('../controllers/usersCtrl');
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
 router.delete('/UserProfileEdit/:userId',auth,userCtrl.deleteAccount);
-router.delete('/UserProfile/Admin/:userId',userCtrl.deleteAccountbyAdmin)
-router.put('/UserProfileEdit/avatar/:userId',multer,userCtrl.deleteImageProfil)   
-router.get('/UserProfile/Admin',userCtrl.getAllUsers)
+router.delete('/UserProfile/Admin/:userId',authAdmin,userCtrl.deleteAccountbyAdmin)
+router.put('/UserProfileEdit/avatar/:userId',auth,multer,userCtrl.deleteImageProfil)   
+router.get('/UserProfile/Admin',authAdmin,userCtrl.getAllUsers)
 router.get('/UserProfileEdit/:userId',auth,userCtrl.getOneUser)
 router.post('/logout',userCtrl.logout)
 router.put('/UserProfileEdit/:userId',auth,multer,userCtrl.updateUser)
